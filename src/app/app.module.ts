a2db20e27f0246546c780bc1f8686e17e5488a7d
Ralf Schneider <ralf.schneider@raiffeisen.ch>
2019-05-01 16:43:52 +0200

    Abschnitt 2, Lektion 17 - Using Custom Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
 import {ServerComponent} from './server/server.component';
 
 @NgModule({
