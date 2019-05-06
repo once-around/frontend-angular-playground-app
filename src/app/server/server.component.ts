6a0670f8394f2aa3899c2e15ae8fb7bb06c90e8b
Ralf Schneider <ralf.schneider@raiffeisen.ch>
2019-05-06 12:18:39 +0200

    Abschnitt 2, Lektion 24 - String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-server',
+  selector: 'app-servers',
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
